1692da34a45d6d69ccfaee8a004ffe8f46fbe64d
Onur Gumus <onur@outlook.com.tr>
2023-07-23 15:39:16 +0200

    failwith versions

--- src/Client/App.fs
+++ src/Client/App.fs
@@ -11,17 +11,19 @@ let testWrapper = document.querySelector(".test-wrapper")  :?> HTMLElement
 let testArea = document.querySelector("#test-area") :?> HTMLTextAreaElement
 let originText = document.querySelector("#origin-text p").innerHTML
 let resetButton = document.querySelector("#reset") :?> HTMLButtonElement
-let theTimer = document.querySelector(".timer") :?> HTMLElement
+let theTimer : HTMLElement = failwith "get timer element"
 
 let model = 
     { Status = Initial; 
         CurrentText = ""; 
         TargetText = originText; 
-        Time =[0;0;0;0] } : TypingModel
+        Time = failwith "set initial time"
+    } : TypingModel
+        
 let viewTime (timer : Time) =
     let leadingZero section =
         if (section <= 9) then
-            "0" + section.ToString()
+            failwith "add leading zero"
         else
             section.ToString()
     let currentTime = leadingZero(timer.[0]) + ":" + leadingZero(timer.[1]) + ":" + leadingZero(timer.[2]);
@@ -36,24 +38,19 @@ let view {Status = status ; Time = time} (dispatcher: MailboxProcessor<Message>)
     | Initial ->
         testArea.value <- ""
         theTimer.innerHTML <- "00:00:00"
-       // testWrapper.style.borderColor <- "grey"
         stopTimer()
     | JustStarted ->
-        if !!(window?myInterval) |> isNull then
-            let interval = window.setInterval  ((fun () -> dispatcher.Post Tick), 10, [])
-            window?myInterval <- interval
+        failwith "clear myInterval"
     | Correct ->
         testWrapper?style?borderColor <- "#65CCf3"
-        //()
-        //testWrapper.
 
     | Wrong ->
-        testWrapper?style?borderColor <- "#E95D0F"
-       //()
-
+        // change border color
+        failwith "Not implemented"
     | Complete ->
         testWrapper?style?borderColor <- "#429890"
-        stopTimer()
+        // stop timer
+        failwith "Change the border color"
 
     viewTime time
 
@@ -65,7 +62,8 @@ let rec dispatcher = MailboxProcessor<Message>.Start(fun inbox->
     // the message processing function
     let rec messageLoop (model : TypingModel) = async{
         // read a message
-        let! msg = inbox.Receive()
+       
+        let! msg = failwith "Read Message"
         // process a message
         let newModel = update model msg
         view newModel dispatcher
@@ -77,4 +75,5 @@ let rec dispatcher = MailboxProcessor<Message>.Start(fun inbox->
 
 testArea.addEventListener("keyup", fun e -> dispatcher.Post (TextUpdated !!(e.target?value)) |> ignore)
 testArea.addEventListener("keypress", fun _ -> dispatcher.Post (KeyPress) |> ignore)
-resetButton.addEventListener("click", fun _ -> dispatcher.Post (StartOver) |> ignore)
+
+failwith "Add event listener for reset button"
--- src/Client/Core.fs
+++ src/Client/Core.fs
@@ -8,21 +8,22 @@ let updateTime (timer:int list) =
     let t0 =  (t3/100./60.) |> Math.Floor
     let t1 =  (t3/100. - t0 * 60.) |> Math.Floor
     let t2 =  (t3 - t1 * 100.- t0 * 6000.) |> Math.Floor
-    [int t0;int t1;int t2; int t3]
+    failwith "return time as a list"
 
 
 
 let update (model : TypingModel) = function
     | Tick -> { model with Time = updateTime model.Time}
-    | StartOver ->  {model with Status = Initial; Time = [0;0;0;0]; CurrentText = ""}
+    | StartOver ->  
+        failwith "Reset the timer and the text area and everything"
     | KeyPress when model.Status = Initial -> { model with Status = JustStarted}
     | TextUpdated text ->
         let model = {model with CurrentText = text}
         let originTextMatch = model.TargetText.Substring(0,model.CurrentText.Length);
         if model.CurrentText = model.TargetText then
-            {model with Status = Complete}
+            failwith "decide status"
         else if model.CurrentText = originTextMatch then
-            { model with Status = Correct}
+            failwith "decide status"
         else
-            {model with Status = Wrong}
+            failwith "decide status"
     | _ -> model
