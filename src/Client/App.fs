309c204c7c7b7dc8d871fa7bd2bd7bad871c327a
Onur Gumus <onur@outlook.com.tr>
2023-10-28 22:57:21 +0200

    init lit

--- src/Client/App.fs
+++ src/Client/App.fs
@@ -4,76 +4,170 @@ open Fable.Core.JsInterop
 open Fable.Import
 open Browser.Types
 open Browser.Dom
-open Model
-open Core
 
-let testWrapper = document.querySelector(".test-wrapper")  :?> HTMLElement
-let testArea = document.querySelector("#test-area") :?> HTMLTextAreaElement
-let originText = document.querySelector("#origin-text p").innerHTML
-let resetButton = document.querySelector("#reset") :?> HTMLButtonElement
-let theTimer : HTMLElement = failwith "get timer element"
+open MVU.Model
+open MVU.Core
 
-let model = 
-    { Status = Initial; 
-        CurrentText = ""; 
-        TargetText = originText; 
-        Time = failwith "set initial time"
-    } : TypingModel
+open Elmish
+open Lit.Elmish
+
+
+module Program =
+    /// <summary>
+    /// Program with user-defined orders instead of usual command.
+    /// Orders are processed by <code>execute</code> which can dispatch messages,
+    /// called in place of usual command processing.
+    /// </summary>
+    let mkHiddenProgramWithOrderExecute
+            (init: 'arg' -> 'model * 'order)
+            (update: 'msg -> 'model -> 'model * 'order)
+            (execute: 'order -> Dispatch<'msg> -> unit) =
+        let convert (model, order) = 
+            model, order |> execute |> Cmd.ofSub
+        Program.mkHidden
+            (init >> convert)
+            (fun msg model -> update msg model |> convert)
+            
+    let mkProgramWithOrderExecute
+            (init: 'arg' -> 'model * 'order)
+            (update: 'msg -> 'model -> 'model * 'order)
+            (view: 'model -> Dispatch<'msg> ->'view)
+            (execute: 'order -> Dispatch<'msg> -> unit) =
+        let convert (model, order) = 
+            model, order |> execute |> Cmd.ofSub
+
+        Program.mkProgram
+            (init >> convert)
+            (fun msg model -> update msg model |> convert)
+            view
+// let testWrapper = document.querySelector(".test-wrapper")  :?> HTMLElement
+// let testArea = document.querySelector("#test-area") :?> HTMLTextAreaElement
+// let originText = document.querySelector("#origin-text p").innerHTML
+// let resetButton = document.querySelector("#reset") :?> HTMLButtonElement
+// let theTimer : HTMLElement = failwith "get timer element"
+
+// let model = 
+//     { Status = Initial; 
+//         CurrentText = ""; 
+//         TargetText = originText; 
+//         Time = failwith "set initial time"
+//     } : TypingModel
         
-let viewTime (timer : Time) =
-    let leadingZero section =
-        if (section <= 9) then
-            failwith "add leading zero"
-        else
-            section.ToString()
-    let currentTime = leadingZero(timer.[0]) + ":" + leadingZero(timer.[1]) + ":" + leadingZero(timer.[2]);
-    theTimer.innerHTML <- currentTime;
+// let viewTime (timer : Time) =
+//     let leadingZero section =
+//         if (section <= 9) then
+//             failwith "add leading zero"
+//         else
+//             section.ToString()
+//     let currentTime = leadingZero(timer.[0]) + ":" + leadingZero(timer.[1]) + ":" + leadingZero(timer.[2]);
+//     theTimer.innerHTML <- currentTime;
+
+// let stopTimer () =
+//     window.clearInterval !!(window?myInterval)
+//     window?myInterval <- null
+
+// let view {Status = status ; Time = time} (dispatcher: MailboxProcessor<Message>) =
+//     match status with
+//     | Initial ->
+//         testArea.value <- ""
+//         theTimer.innerHTML <- "00:00:00"
+//         stopTimer()
+//     | JustStarted ->
+//         failwith "clear myInterval"
+//     | Correct ->
+//         testWrapper?style?borderColor <- "#65CCf3"
+
+//     | Wrong ->
+//         // change border color
+//         failwith "Not implemented"
+//     | Complete ->
+//         testWrapper?style?borderColor <- "#429890"
+//         // stop timer
+//         failwith "Change the border color"
+
+//     viewTime time
+
+
+
+// #nowarn "40"
+// let rec dispatcher = MailboxProcessor<Message>.Start(fun inbox->
+
+//     // the message processing function
+//     let rec messageLoop (model : TypingModel) = async{
+//         // read a message
+       
+//         let! msg = failwith "Read Message"
+//         // process a message
+//         let newModel = update model msg
+//         view newModel dispatcher
+//         // loop to top
+//         return! messageLoop newModel}
+
+//     // start the loop
+//     messageLoop model)
+
+// testArea.addEventListener("keyup", fun e -> dispatcher.Post (TextUpdated !!(e.target?value)) |> ignore)
+// testArea.addEventListener("keypress", fun _ -> dispatcher.Post (KeyPress) |> ignore)
+
+// failwith "Add event listener for reset button"
+
+open Elmish
+open Lit
+open Lit.Elmish
 
 let stopTimer () =
     window.clearInterval !!(window?myInterval)
     window?myInterval <- null
 
-let view {Status = status ; Time = time} (dispatcher: MailboxProcessor<Message>) =
-    match status with
-    | Initial ->
-        testArea.value <- ""
-        theTimer.innerHTML <- "00:00:00"
-        stopTimer()
-    | JustStarted ->
-        failwith "clear myInterval"
-    | Correct ->
-        testWrapper?style?borderColor <- "#65CCf3"
+let startTimer dispatch =
+    if !!(window?myInterval) |> isNull then
+        let interval = window.setInterval  ((fun () -> dispatch Tick), 10, [])
+        window?myInterval <- interval
 
-    | Wrong ->
-        // change border color
-        failwith "Not implemented"
-    | Complete ->
-        testWrapper?style?borderColor <- "#429890"
-        // stop timer
-        failwith "Change the border color"
+let rec execute  order dispatch =
+    match order with
+    | NoEffect -> ()
+    | StartTimer -> startTimer dispatch
+    | StopTimer -> stopTimer ()
+let viewTime (timer : Time) =
+    let leadingZero section =
+        if (section <= 9) then
+            "0" + section.ToString()
+        else
+            section.ToString()
+    leadingZero(timer.[0]) + ":" + leadingZero(timer.[1]) + ":" + leadingZero(timer.[2]);
+ 
+[<HookComponent>]
+let view (model: TypingModel) dispatch =
+   
+    html $"""
+        <article class="intro">
+            <p>This is a typingg test. Your goal is to duplicate the provided text, EXACTLY, in the field below. The timer starts when you start typing, and only stops when you match this text exactly. Good Luck!</p>
+        </article>
+        <section class="test-area">
+            <div id="origin-text">
+                <p>This is a typing test.</p>
+            </div>
 
-    viewTime time
+            <div class="test-wrapper">
+                <textarea id="test-area" @keyup={Ev( fun e -> dispatch (TextUpdated !!(e.target?value)))} 
+                    @keypress={Ev( fun _ -> dispatch (KeyPress))}
+                    name="textarea" rows="6" .value={model.CurrentText} placeholder="The clock starts when you start typing."></textarea>
+            </div>
+
+            <div class="meta">
+                <section id="clock">
+                    <div class="timer">{viewTime(model.Time)}</div>
+                </section>
+
+                <button id="reset" @click={Ev( fun e -> dispatch (StartOver))} >Start over</button>
+            </div>
+        </section> 
+            """
 
 
 
-#nowarn "40"
-let rec dispatcher = MailboxProcessor<Message>.Start(fun inbox->
-
-    // the message processing function
-    let rec messageLoop (model : TypingModel) = async{
-        // read a message
-       
-        let! msg = failwith "Read Message"
-        // process a message
-        let newModel = update model msg
-        view newModel dispatcher
-        // loop to top
-        return! messageLoop newModel}
-
-    // start the loop
-    messageLoop model)
-
-testArea.addEventListener("keyup", fun e -> dispatcher.Post (TextUpdated !!(e.target?value)) |> ignore)
-testArea.addEventListener("keypress", fun _ -> dispatcher.Post (KeyPress) |> ignore)
-
-failwith "Add event listener for reset button"
+Program.mkProgramWithOrderExecute MVU.Core.init (MVU.Core.update) view execute
+|> Program.withLitOnElement (document.querySelector "main")
+|> Program.withConsoleTrace
+|> Program.run
---
+++ src/MVU/Library.fs
@@ -0,0 +1,60 @@
+﻿namespace MVU
+
+module Model=
+
+    type Time = int list
+
+    type Status =
+        | Initial
+        | JustStarted
+        | Complete
+        | Wrong
+        | Correct
+
+    type Message = 
+        | Tick
+        | StartOver
+        | KeyPress
+        | TextUpdated of string
+
+    type TypingModel = { Time : Time; Status : Status; CurrentText : string; TargetText : string }
+
+    type SideEffect = 
+        | NoEffect
+        | StartTimer
+        | StopTimer
+
+module Core=
+
+    open Model
+    open System
+    let init () = 
+            {   Status = Initial; 
+                CurrentText = ""; 
+                TargetText = "This is a typing test.";
+                Time = [0;0;0;0] } ,NoEffect
+        
+    let updateTime (timer:int list) =
+        let t3 =  (timer.[3] + 1) |> float
+        let t0 =  (t3/100./60.) |> Math.Floor
+        let t1 =  (t3/100. - t0 * 60.) |> Math.Floor
+        let t2 =  (t3 - t1 * 100.- t0 * 6000.) |> Math.Floor
+        [int t0;int t1;int t2; int t3]
+
+
+
+    let update (msg:Message) (model : TypingModel) =
+        match msg with
+        | Tick -> { model with Time = updateTime model.Time}, NoEffect
+        | StartOver ->  {model with Status = Initial; Time = [0;0;0;0]; CurrentText = ""},StopTimer
+        | KeyPress when model.Status = Initial -> { model with Status = JustStarted},StartTimer
+        | TextUpdated text ->
+            let model = {model with CurrentText = text}
+            let originTextMatch = model.TargetText.Substring(0,model.CurrentText.Length);
+            if model.CurrentText = model.TargetText then
+                {model with Status = Complete},StopTimer
+            else if model.CurrentText = originTextMatch then
+                { model with Status = Correct},NoEffect
+            else
+                {model with Status = Wrong},NoEffect
+        | _ -> model,NoEffect
